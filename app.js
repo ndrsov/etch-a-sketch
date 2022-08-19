733091b4040d4621b8f637d94913f2b4cb78b188
Andres Soria <andres.zov@gmail.com>
2022-08-19 17:34:16 -0500

    Change paint area from flexbox to a grid system

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const sketch = document.querySelector("#sketch");
 
-const gridSize = 16;
-
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
 function setupGrid(size) {
+  sketch.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  sketch.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   for (let i = 0; i < size * size; i++) {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
