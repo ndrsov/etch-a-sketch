005f9bb40a7f118446ef50b2e10eb196a724bf70
Andres Soria <andres.zov@gmail.com>
2022-08-18 20:00:13 -0500

    Grid system improved

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const sketch = document.querySelector("#sketch");
-const generateBtn = document.querySelector("#generateBtn");
 
 const gridSize = 16 * 16;
 
-generateBtn.addEventListener("click", createPixelGrid);
-
-function createPixelGrid() {
-  for (let i = 0; i < gridSize; i++) {
-    const pixel = document.createElement("div");
-    pixel.classList.add("pixel");
-    sketch.appendChild(pixel);
-  }
+for (let i = 0; i < gridSize; i++) {
+  const pixel = document.createElement("div");
+  pixel.classList.add("pixel");
+  pixel.addEventListener("click", () => {
+    pixel.style.backgroundColor = "black";
+  });
+  sketch.appendChild(pixel);
 }
