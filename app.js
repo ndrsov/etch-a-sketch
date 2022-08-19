7e73e8f933f88de47cd15cbb4ad9847038d0de61
Andres Soria <andres.zov@gmail.com>
2022-08-19 16:47:36 -0500

    Add function to paint grid only when mouse is presed

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const sketch = document.querySelector("#sketch");
 
-const gridSize = 16 * 16;
+const gridSize = 16;
 
-for (let i = 0; i < gridSize; i++) {
-  const pixel = document.createElement("div");
-  pixel.classList.add("pixel");
-  pixel.addEventListener("click", () => {
-    pixel.style.backgroundColor = "black";
-  });
-  sketch.appendChild(pixel);
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function setupGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.addEventListener("mouseover", paint);
+    pixel.addEventListener("mousedown", paint);
+    sketch.appendChild(pixel);
+  }
 }
+
+function paint(e) {
+  if (e.type === "mouseover" && !mouseDown) {
+    return;
+  } else {
+    e.target.style.backgroundColor = "black";
+  }
+}
+
+window.onload = () => {
+  setupGrid(16);
+};
